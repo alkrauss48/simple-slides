d21e924d9b7ad8350c94b11df172f10723d62640
aaron <alkrauss48@gmail.com>
2023-11-19 14:38:37 -0600

    feat: implement initial browsershot workflow
    Note: the image generation doesn't look right yet.

--- app/Filament/Resources/PresentationResource/Pages/EditPresentation.php
+++ app/Filament/Resources/PresentationResource/Pages/EditPresentation.php
@@ -6,6 +6,7 @@ use App\Filament\Resources\PresentationResource;
 use App\Models\Presentation;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Spatie\Browsershot\Browsershot;
 
 class EditPresentation extends EditRecord
 {
@@ -21,6 +22,20 @@ class EditPresentation extends EditRecord
                 ]))
                 ->icon('heroicon-o-presentation-chart-bar')
                 ->openUrlInNewTab(),
+            Actions\Action::make('Generate Thumbnail')
+                ->icon('heroicon-o-camera')
+                ->action(function (Presentation $record) {
+                    // Browsershot::url('https://example.com')
+                    Browsershot::url(route('presentations.show', [
+                        'user' => $record->user->username,
+                        'slug' => $record->slug,
+                    ]))
+                        ->windowSize(1200, 630)
+                        ->noSandbox()
+                        ->setScreenshotType('jpeg', 80)
+                        ->save(storage_path("app/public/{$record->user->username}-{$record->slug}.jpg"));
+                    // $set('price', $state);
+                }),
             Actions\DeleteAction::make(),
             Actions\ForceDeleteAction::make(),
             Actions\RestoreAction::make(),

817a2d0e4cdc1be0b0948dbc470fd0503464b981
aaron <alkrauss48@gmail.com>
2023-11-18 22:38:29 -0600

    style: update auth screen logo

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -7,7 +7,7 @@ import { Link } from '@inertiajs/vue3';
     <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
         <div>
             <Link href="/">
-                <ApplicationLogo class="w-20 h-20 fill-current text-gray-500" />
+                <ApplicationLogo class="mb-4" />
             </Link>
         </div>
 

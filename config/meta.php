931c82b722699f2ecb4feb5622365782672421c1
aaron <alkrauss48@gmail.com>
2024-02-16 10:42:14 -0600

    chore: auto-format the meta config

--- config/meta.php
+++ config/meta.php
@@ -1,11 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Facade;
-use Illuminate\Support\ServiceProvider;
-
 return [
-    'base_description' =>
-        'Simple Slides is a responsive and text-first presentation ' .
+    'base_description' => 'Simple Slides is a responsive and text-first presentation '.
         'tool that keeps your audience engaged.',
     'base_og_image' => 'https://simpleslides.dev/images/simple-slides-og.jpg',
     'twitter_handle' => '@thecodeboss',

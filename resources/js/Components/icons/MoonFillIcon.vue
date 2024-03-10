b2cd5ad84073248050b08c60ed77d2c0bd98bbb3
aaron <alkrauss48@gmail.com>
2024-03-10 00:06:39 -0600

    refactor: rebuild settings bottom nav
    Addresses #12

---
+++ resources/js/Components/icons/MoonFillIcon.vue
@@ -0,0 +1,6 @@
+<template>
+    <svg class="w-8 h-8" version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
+        <title>Toggle Dark Mode</title>
+        <path fill="currentColor" d="M24.633 22.184c-8.188 0-14.82-6.637-14.82-14.82 0-2.695 0.773-5.188 2.031-7.363-6.824 1.968-11.844 8.187-11.844 15.644 0 9.031 7.32 16.355 16.352 16.355 7.457 0 13.68-5.023 15.648-11.844-2.18 1.254-4.672 2.028-7.367 2.028z"></path>
+    </svg>
+</template>

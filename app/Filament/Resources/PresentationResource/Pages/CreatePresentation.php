ef040af7595f68dea820d5cbabc1032231e56891
aaron <alkrauss48@gmail.com>
2024-09-03 22:24:39 -0500

    refactor: simplify CreatePresentation actions UX

--- app/Filament/Resources/PresentationResource/Pages/CreatePresentation.php
+++ app/Filament/Resources/PresentationResource/Pages/CreatePresentation.php
@@ -3,9 +3,20 @@
 namespace App\Filament\Resources\PresentationResource\Pages;
 
 use App\Filament\Resources\PresentationResource;
+use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreatePresentation extends CreateRecord
 {
     protected static string $resource = PresentationResource::class;
+
+    protected static bool $canCreateAnother = false;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\Action::make('create')
+                ->action('create'),
+        ];
+    }
 }

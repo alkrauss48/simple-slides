ef0ecc2ecd5f87693cf0c460be36080cd2db0014
aaron <alkrauss48@gmail.com>
2025-03-06 13:43:41 -0600

    refactor: swap auth logic for filament workflow
    includes updating tests

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Pages\Auth\Register;
 use App\Livewire\UsernameComponent;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -28,6 +29,9 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->registration(register::class)
+            ->passwordreset()
+            ->emailVerification()
             ->colors([
                 'primary' => Color::Amber,
             ])

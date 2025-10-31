3a01e01767ae3dca29c9b8ba45eb8e6985eeb38d
aaron <alkrauss48@gmail.com>
2025-10-31 01:33:14 -0500

    refactor: update dark mode icon on popup settings menu

--- resources/js/Components/SettingsMenu.vue
+++ resources/js/Components/SettingsMenu.vue
@@ -3,8 +3,6 @@ import { ref, computed, watch } from 'vue'
 import { Link, router } from '@inertiajs/vue3'
 
 import CogIcon from '@/Components/icons/CogIcon.vue';
-import MoonFillIcon from '@/Components/icons/MoonFillIcon.vue';
-import MoonStrokeIcon from '@/Components/icons/MoonStrokeIcon.vue';
 import Presentation from '@/interfaces/presentation.ts';
 import { VisualMode, isDarkMode } from '@/enums/visualMode.ts';
 import { getVisualMode, setVisualMode } from '@/utils/handleVisualMode.ts';
@@ -219,17 +217,25 @@ watch(isOpen, (newValue) => {
                         </span>
                         <button
                             @click.stop="darkMode = !darkMode"
-                            class="
-                                text-3xl
-                                text-gray-600 dark:text-gray-300
-                                hover:text-gray-900 dark:hover:text-gray-100
-                                focus:text-gray-900 dark:focus:text-gray-100
-                                transition-colors
-                            "
+                            type="button"
+                            role="switch"
+                            :aria-checked="darkMode"
                             :aria-label="darkMode ? 'Disable Dark Mode' : 'Enable Dark Mode'"
+                            class="
+                                relative inline-flex h-5 w-9 flex-shrink-0 cursor-pointer
+                                rounded-full border-2 border-transparent
+                                transition-colors duration-200 ease-in-out
+                                focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2
+                                dark:focus:ring-offset-gray-800
+                                bg-gray-200 dark:bg-gray-600
+                            "
                         >
-                            <MoonStrokeIcon v-if="!darkMode" />
-                            <MoonFillIcon v-else />
+                            <span
+                                :class="[
+                                    'pointer-events-none inline-block h-4 w-4 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out',
+                                    darkMode ? 'translate-x-4' : 'translate-x-0'
+                                ]"
+                            />
                         </button>
                     </div>
 
